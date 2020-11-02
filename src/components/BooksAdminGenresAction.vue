754104c1076c5d6a8b1d23a058a0abefb1f0ac2b
cellyasirot <cellya.sirot@gmail.com>
2020-11-02 17:47:11 +0100

    add genre

--- src/components/BooksAdminGenresAction.vue
+++ src/components/BooksAdminGenresAction.vue
@@ -1,6 +1,35 @@
+<template>
+  <form>
+  <div class="form-group">
+    <label for="exampleInputEmail1">Libellé Genre</label>
+    <input type="text" class="form-control" id="exampleInputEmail1">
+  </div>
+  
+  <button type="submit" class="btn btn-primary" @click='createGenre'>Submit</button>
+</form>
+</template>
+
+
+
 <script>
+
+import axios from "axios"
+
 export default {
   name: 'BooksAdminGenresAction',
+
+  methods:{
+    async createGenre()
+    {
+      const genre = { id:"", label: "Vue POST Request Example", icon: "" };
+      const response = await axios.post("http://localhost:3000/genres", genre);
+      this.genreId = response.data.id;
+    }
+  },
+
+  created() {
+    
+  }
 }
 </script>
 
